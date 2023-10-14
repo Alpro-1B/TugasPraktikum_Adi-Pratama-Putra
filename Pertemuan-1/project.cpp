ce01ac0625d6da863ea9cd2f948ef66a01b93798
pratamaputraadi <adiprtamap240@gmail.com>
2023-10-14 19:25:57 +0700

    menambahkan Pertemuan-1

---
+++ Pertemuan-1/project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout <<"---------------------------------------------------------"<<endl;
+    cout <<"Nama : Adi Pratama Putra"<<endl<<endl;
+    cout <<"NPM : 2310631170062"<<endl<<endl;
+    cout <<"Kelas : B - Informatika"<<endl<<endl;
+    cout <<"Cita-Cita : Ahli Cuaca dan Pegawai Tetap BMKG"<<endl<<endl;
+    cout <<"Motivasi Masuk Informatika : Rekomendasi Orang Tua & Guru"<<endl;
+    cout <<"---------------------------------------------------------"<<endl;
+    return 0;
+}
